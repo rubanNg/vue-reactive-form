5335cda34c99864abf6426f45255eb0e8bdc67c8
Александр Рубан <ruban.mail1991@gmail.com>
2022-08-01 12:17:32 +0200

    event

--- src/classes/reactive-form.ts
+++ src/classes/reactive-form.ts
@@ -1,20 +1,25 @@
 import { AbstractControl } from "./abstract-conrol";
-import { defineProperties, find, isArray, toArray, toRecord } from "../utils";
+import { defineProperties, find, isArray, isObject, toArray, toRecord } from "../utils";
 import { ref } from "vue";
-import { ArrayState, ObjectState } from "../types";
 
+
+enum Type {
+  Array,
+  Object
+}
 
 export class ReactiveForm {
 
-  #structureType: 'array' | 'object' = null;
-  #_controls = ref<ObjectState>(null)
+  #structureType: Type = null;
+  #_controls = ref<Record<string, AbstractControl>>(null)
 
-  get controls(): ObjectState | ArrayState { 
-    return this.#getControls() 
+  get controls() { 
+    if (this.#structureType === Type.Object) return this.#_controls.value as Record<string, AbstractControl>;
+    if (this.#structureType === Type.Array) return toArray<AbstractControl>(this.#_controls.value);
   }
   get value(): any { return this.#getValue(); }
 
-  constructor(controls: ArrayState | ObjectState) {
+  constructor(controls: AbstractControl[] | Record<string, AbstractControl>) {
     this.#configureControls(controls)
   }
 
@@ -22,19 +27,29 @@ export class ReactiveForm {
     return find<AbstractControl>(this, path)
   }
 
+  addControl(control: AbstractControl, name?: string) {
+    if (this.#structureType === Type.Array) {
+      const index = this.#getNextIndex();
+      this.#_controls.value[index] = control;
+    } else {
+      if (!name) throw new Error("The name argument is a required parameter because controls are an object.");
+      this.#_controls.value[name] = control;
+    }
+  }
+
+  #getNextIndex() {
+    return Object.keys(this.#_controls).length.toString();
+  }
+
   #getValue() {
     return Object.keys(this.#_controls.value).reduce((result: any, key) => {
       result[key] = this.#_controls.value[key].value;
       return result;
-    }, this.#structureType === "array" ? [] : {})
+    }, this.#structureType === Type.Array ? [] : {})
   }
 
-  #getControls() {
-    return this.#structureType === 'object' ? this.#_controls.value : toArray(this.#_controls.value);
-  }
-
-  #configureControls(controls: ArrayState | ObjectState) {
-    this.#structureType = isArray(controls) ? 'array' : 'object'
+  #configureControls(controls: AbstractControl[] | Record<string, AbstractControl>) {
+    this.#structureType = isArray(controls) ? Type.Array : Type.Object
     this.#_controls.value = toRecord(controls);
     for (const name in this.#_controls.value) {
       this.#_controls.value[name].setForm(this);
