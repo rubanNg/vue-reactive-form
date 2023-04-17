66a653c15c83ce700c0cb3412d9fbc9a432db967
rubanNg <ruban.mail1991@gmail.com>
2023-04-18 01:13:08 +0200

    add examples

---
+++ example/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <h2>Examples</h2>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+    </ul>
+  </div>
+</template>
+
+
+
